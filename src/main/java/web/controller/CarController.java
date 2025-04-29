99c14ee7aa0bb2f594ca0490524798dadac5cd31
gavrilsenpai <19oxlopkov99@gmail.com>
2025-04-29 13:39:53 +0300

    Сделал правки

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,28 +1,30 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
 import web.service.CarService;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/cars")
 public class CarController {
-    private final CarService carService = new CarService();
 
-    @GetMapping(produces = "text/html; charset=UTF-8")
-    public String getCars(@RequestParam(value = "count", required = false) Integer count, Model model) {
-        if (count == null) {
-            count = 5;
-        }
+    private final CarService carService;
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping
+    public String showCars(@RequestParam(value = "count", defaultValue = "5") int count,
+                           Model model) {
         model.addAttribute("cars", carService.getCars(count));
         return "cars";
     }
 }
+
 
